94b22c5a360bbf0a0815bb68fc27ac781f6d29cd
t3h <a230385707@gmail.com>
2025-03-23 17:38:40 +0100

    creating mockup components

---
+++ src/components/TheHero.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+  <div class="hero bg-base-100">
+    <div class="flex flex-col">
+      <div class="hero-content flex-col lg:flex-row-reverse mb-8">
+        <!-- visuel/icon -->
+        <div class="lg:w-1/2 flex justify-end">
+          <Icon icon="emojione:shopping-cart" class="text-9xl max-w-sm" />
+        </div>
+        <!-- contenu texte -->
+        <div class="lg:w-1/2">
+          <h1 class="text-5xl font-bold">
+            Welcome back <span class="text-secondary">{user} </span>!
+          </h1>
+          <h2 class="font-style: italic">
+            I'll remember what you forgot since... <span class="font-bold text-secondary">wait</span>, what were we talking about?
+          </h2>
+          <p class="py-6">
+            This app remembers your shopping lists so you don't have to pretend to,
+            because who <span class="text-primary font-bold underline">really</span> needs neurons when you have notifications?
+          </p>
+        </div>
+      </div>
+      <!-- boutons en dehors du hero -->
+      <div class="flex justify-center w-full">
+        <button class="btn btn-primary mr-2">
+          Start Over
+        </button>
+        <button class="btn btn-secondary">
+          Resume Ongoing
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="navbar bg-base-200 shadow-sm">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+        </div>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+        >
+          <li><a>Homepage</a></li>
+          <li><a>Saved Shoplists</a></li>
+          <li><a>Schedules</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="navbar-center">
+      <a class="btn btn-ghost text-xl">My Shoplist</a>
+    </div>
+    <div class="navbar-end">
+      <div class="justify-between">
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+            <div class="indicator">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+              <span class="badge badge-sm indicator-item badge-accent">0</span><!-- TODO: make this dynamic -->
+            </div>
+          </div>
+          <div
+            tabindex="0"
+            class="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
+          >
+            <div class="card-body">
+              <span class="text-lg font-bold">8 Items</span><!-- TODO: make this dynamic -->
+              <span class="text-info">Subtotal: $999</span><!-- TODO: make this dynamic -->
+              <div class="card-actions">
+                <button class="btn btn-primary btn-block">
+                  View cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
