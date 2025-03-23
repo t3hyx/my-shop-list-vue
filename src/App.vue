65a8d76effbbc678963f1dafe20bb8297cc4f276
t3h <a230385707@gmail.com>
2025-03-23 17:39:02 +0100

    adding mockup components to app.vue + cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
+import AlertBar from './components/AlertBar.vue'
+import TheFooter from './components/TheFooter.vue'
+import TheHero from './components/TheHero.vue'
+import TheNavbar from './components/TheNavbar.vue'
 </script>
 
 <template>
+  <div class="flex flex-col min-h-screen">
+    <TheNavbar />
+    <AlertBar />
+    <TheHero class="flex-grow" />
+    <TheFooter />
+  </div>
 </template>
 
 <style scoped>
