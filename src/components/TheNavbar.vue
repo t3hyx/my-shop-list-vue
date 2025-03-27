83df7877a0b42d273e60382fb7047d79f78dd913
t3h <a230385707@gmail.com>
2025-03-27 21:36:32 +0100

    making the navbar cart badge dynamic

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import HomeView from '../views/HomeView.vue'
+import { Icon } from '@iconify/vue'
+import { useShoppingList } from '../store/useShoppingList'
+
+const { totalShopItems /* filteredShopItems */ } = useShoppingList()
 </script>
 
 <template>
@@ -13,7 +16,11 @@ import HomeView from '../views/HomeView.vue'
           tabindex="0"
           class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
         >
-          <li><RouterLink :to="HomeView" /></li>
+          <li>
+            <RouterLink :to="{ name: 'home' }">
+              Returning Home
+            </RouterLink>
+          </li>
           <li><a>Saved Shoplists</a></li>
           <li><a>Schedules</a></li>
         </ul>
@@ -31,8 +38,8 @@ import HomeView from '../views/HomeView.vue'
         <div class="dropdown dropdown-end">
           <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
             <div class="indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
-              <span class="badge badge-sm indicator-item badge-accent">0</span><!-- TODO: make this dynamic -->
+              <Icon icon="ion:cart" class="size-8" />
+              <span class="badge badge-sm indicator-item badge-accent">{{ totalShopItems }}</span>
             </div>
           </div>
           <div
@@ -40,8 +47,8 @@ import HomeView from '../views/HomeView.vue'
             class="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
           >
             <div class="card-body">
-              <span class="text-lg font-bold">8 Items</span><!-- TODO: make this dynamic -->
-              <span class="text-info">Subtotal: $999</span><!-- TODO: make this dynamic -->
+              <span class="text-lg font-bold">{{ totalShopItems }} Items</span>
+              <span class="text-info">Subtotal: $999 (not implemented)</span><!-- TODO: make this dynamic -->
               <div class="card-actions">
                 <button class="btn btn-primary btn-block">
                   View cart
